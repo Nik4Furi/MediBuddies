b99bede21f5d296c1ff18cc226032243ce7b33e2
MediBuddi <medibuddy@gmail.com>
2022-05-15 17:40:01 +0530

    We are create the partials and In views folder add the index.js and another pages , You can start  creating the error page

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 //DECLARE THE VARIABLES
 const { urlencoded } = require('body-parser');
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
-const port = 8000;
+const port = process.env.PORT||8000;
 const path = require('path');
 const bodyParser = require('body-parser')
-const viewPath = path.join(__dirname,'views');
+const viewPath = path.join(__dirname,'/templates/views');
+const partialsPath = path.join(__dirname,'/templates/Partials');
 
 //EXPRESS SPECIFIC STURFF
-
 app.use('/static',express.static('static'));
 app.use(express.urlencoded());
 
-//PUG SPEDIFIC STUFF
-app.set('view engine','pug');
+//HBS SPEDIFIC STUFF
+app.set('view engine','hbs');
 app.set('views',viewPath);
+hbs.registerPartials(partialsPath);
 
 //ENDPOINTS
 app.get('/',(req,res)=>{
     // res.send("Hello, is this home page?")
-    res.status(200).render('home.pug');
+    res.status(200).render('home',{title:'Home: '});
 })
 app.get('/about',(req,res)=>{ 
     // res.send("Hello, is this about page?")
-    res.status(200).render('about.pug');
+    res.status(200).render('about',{title:'About: '});
+})
+app.get('*',(req,res)=>{ 
+    // res.send("Hello, is this about page?")
+    res.status(404).render('errorPage');
 })
 
 //LISTING THE PORT
